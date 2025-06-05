8d45577371b91e7207812aac84d7a7c0ccd7723c
FrotaLucas <lucasdiasfrota@gmail.com>
2025-06-05 17:41:33 +0200

    refactoring folder client/services

--- BlazorEcommerce_V2/Client/Services/AddressService/IAddressService.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace BlazorEcommerce_V2.Client.Services.AddressService
-{
-    public interface IAddressService
-    {
-        Task<Address> GetAddress();
-        Task<Address> AddOrUpdateAddress(Address address);
-    }
-}
