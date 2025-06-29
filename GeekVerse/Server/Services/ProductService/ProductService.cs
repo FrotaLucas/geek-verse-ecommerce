02eff008c4d73d5f519ff77ae4b086eb9f85efe1
FrotaLucas <lucasdiasfrota@gmail.com>
2025-06-29 20:10:58 +0200

    dele unused comments

--- GeekVerse/Server/Services/ProductService/ProductService.cs
+++ GeekVerse/Server/Services/ProductService/ProductService.cs
@@ -284,66 +284,6 @@ namespace GeekVerse.Server.Services.ProductService
                 variantToBeUpdated.Visible = variant.Visible;
             }
 
-            //foreach (var variant in product.Variants)
-            //{
-            //    var dbVariant = await _context.ProductVariant
-            //        .SingleOrDefaultAsync(v => v.ProductId == variant.ProductId &&
-            //        v.ProductTypeId == variant.ProductTypeId);
-
-            //    if (dbVariant == null)
-            //    {
-            //        dbVariant = new ProductVariant();
-            //        //dbVariant.ProductType = null; //preciso disso ?
-            //        //caso criando novo Variant
-            //        if (variant.IsNew)
-            //        {
-            //            // ainda nao existe variant.ProductTypeId;
-            //            _context.ProductVariant.Add(variant);
-            //        }
-
-            //        //caso atualizando Variant como
-            //        else
-            //        {
-            //            var listOfVariants = await _context.ProductVariant
-            //                .Where(v => v.ProductId.Equals(product.Id))
-            //                .ToListAsync();
-
-
-
-            //            var variantsToRemove = listOfVariants
-            //                .Where(v => !product.Variants.Any(updatedVariant => 
-            //                    updatedVariant.ProductTypeId.Equals(v.ProductTypeId) &&
-            //                    updatedVariant.Price.Equals(v.Price) && 
-            //                    updatedVariant.OriginalPrice.Equals(v.OriginalPrice) && 
-            //                    updatedVariant.Visible.Equals(v.Visible) &&
-            //                    updatedVariant.Deleted.Equals(v.Deleted)))
-            //                .ToList();
-
-
-            //            _context.ProductVariant.RemoveRange(variantsToRemove);
-            //            await _context.SaveChangesAsync();
-
-            //            variant.ProductType = null;
-            //            _context.ProductVariant.Add(variant);
-            //            await _context.SaveChangesAsync(); //AQUI
-            //            //product
-
-
-            //        }
-
-
-            //    }
-
-            //    //Pq precisa atualizar ProductId tbm ? Depois de atualzar Price e Original Price da variante de um produto,
-            //    //a variante ainda vai continuar se referenciando sobre o mesmo produto!!
-            //    dbVariant.ProductId = variant.ProductId;
-            //    dbVariant.OriginalPrice = variant.OriginalPrice;
-            //    dbVariant.Price = variant.Price;
-            //    dbVariant.Deleted = variant.Deleted;
-            //    dbVariant.Visible = variant.Visible;
-
-            //}
-
             //dbvariante tem escopo restrito no loop. Esse save nao deveria esta dentro do foreach ?
             await _context.SaveChangesAsync();
 
